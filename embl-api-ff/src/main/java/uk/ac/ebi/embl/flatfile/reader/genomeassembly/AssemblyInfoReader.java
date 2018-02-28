1b5954f1ce0e6ee058b47197ecb115032b405753
vijayaraja <vijayaraja@ebi.ac.uk>
2018-02-28 10:51:17 +0000

    Revert "assembly info read by space"
    This reverts commit 1705b3f

--- embl-api-ff/src/main/java/uk/ac/ebi/embl/flatfile/reader/genomeassembly/AssemblyInfoReader.java
+++ embl-api-ff/src/main/java/uk/ac/ebi/embl/flatfile/reader/genomeassembly/AssemblyInfoReader.java
@@ -3,9 +3,6 @@ package uk.ac.ebi.embl.flatfile.reader.genomeassembly;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import org.apache.commons.lang3.StringUtils;
 import uk.ac.ebi.embl.api.entry.genomeassembly.AssemblyInfoEntry;
 import uk.ac.ebi.embl.api.validation.ValidationResult;
@@ -15,8 +12,6 @@ public class AssemblyInfoReader extends GCSEntryReader
 	private final String MESSAGE_KEY_INVALID_FORMAT_ERROR = "invalidlineFormat";
 	private final String MESSAGE_KEY_INVALID_VALUE_ERROR = "invalidfieldValue";
 	private final String MESSAGE_KEY_INVALID_FIELD_ERROR = "invalidfieldName";
-
-	private static Pattern pattern =Pattern.compile("^(\\w+)\\s+(.+)$");
 
 	AssemblyInfoEntry assemblyInfoEntry = null;
 	public AssemblyInfoReader(File file)
@@ -39,14 +34,18 @@ public class AssemblyInfoReader extends GCSEntryReader
 				{
 					continue;
 				}
-
-				Matcher matcher = pattern.matcher(line);
-
-				if(matcher.matches() && matcher.groupCount() == 2)
+				
+				String[] fields = line.trim().split("\\s+",2);
+				int numberOfColumns = fields.length;
+				if (numberOfColumns != 2 )
 				{
-					String field= StringUtils.deleteWhitespace(matcher.group(1).toUpperCase());
+					error(lineNumber,MESSAGE_KEY_INVALID_FORMAT_ERROR,line);
+				}
+				else
+				{
+					String field= StringUtils.deleteWhitespace(fields[0].toUpperCase());
 					field= field.replaceAll("_","").replaceAll("-","").replaceAll("\\.","");			
-					String fieldValue= matcher.group(2).trim();
+					String fieldValue= fields[1];
 					switch(field)
 					{
 					case "ASSEMBLYNAME":
@@ -65,13 +64,13 @@ public class AssemblyInfoReader extends GCSEntryReader
 						if(isInteger(fieldValue))
 						  assemblyInfoEntry.setMinGapLength(new Integer(fieldValue));
 						else
-						  error(lineNumber,MESSAGE_KEY_INVALID_VALUE_ERROR,field,fieldValue);
+						  error(lineNumber,MESSAGE_KEY_INVALID_VALUE_ERROR,fields[0],fieldValue);
 						break;
 					case "MOLECULETYPE":
 						if(isValidMoltype(fieldValue))
 							assemblyInfoEntry.setMoleculeType(fieldValue);
 						else
-						    error(lineNumber,MESSAGE_KEY_INVALID_VALUE_ERROR,field,fieldValue);
+						    error(lineNumber,MESSAGE_KEY_INVALID_VALUE_ERROR,fields[0],fieldValue);
 						break;
 					case "SAMPLE":
 						 assemblyInfoEntry.setSampleId(fieldValue);
@@ -84,8 +83,6 @@ public class AssemblyInfoReader extends GCSEntryReader
 						break;
 					}
 					
-				} else {
-					error(lineNumber,MESSAGE_KEY_INVALID_FORMAT_ERROR,line);
 				}
 				lineNumber++;
 
