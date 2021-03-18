98db6ffa8e166fe98820b687975472775d1c9b39
prabh-t <prabhat@ebi.ac.uk>
2021-03-18 17:02:34 +0000

    address PR#70  comments

--- src/main/java/uk/ac/ebi/embl/flatfile/writer/embl/EmblReducedFlatFileWriter.java
+++ src/main/java/uk/ac/ebi/embl/flatfile/writer/embl/EmblReducedFlatFileWriter.java
@@ -8,6 +8,7 @@ import uk.ac.ebi.embl.api.entry.Entry;
 import uk.ac.ebi.embl.api.entry.feature.Feature;
 import uk.ac.ebi.embl.api.entry.feature.SourceFeature;
 import uk.ac.ebi.embl.api.entry.reference.Reference;
+import uk.ac.ebi.embl.api.validation.ValidationException;
 import uk.ac.ebi.embl.flatfile.EmblTag;
 import uk.ac.ebi.embl.flatfile.writer.EntryWriter;
 import uk.ac.ebi.embl.flatfile.writer.WrapType;
@@ -17,20 +18,16 @@ import uk.ac.ebi.ena.taxonomy.taxon.Taxon;
  */
 public class EmblReducedFlatFileWriter extends EntryWriter {
 
+    public static final String REDUCED_FF_WRITE_FAILED_MISSING_SEQUENCE = "Failed to write reduced flat file. Missing sequence for entry";
+    public static final String REDUCED_FF_WRITE_FAILED_MISSING_CONTIGS = "Failed to write reduced flat file. Missing contigs for CON entry";
+    public static final String REDUCED_FF_WRITE_FAILED_UNEXPECTED_CONTIGS = "Failed to write reduced flat file. Unexpected contigs for non-CON entry";
+
     public EmblReducedFlatFileWriter(Entry entry) {
         super(entry);
         wrapType = WrapType.EMBL_WRAP;
     }
 
-    private boolean excludeSource = true;
-
-    public boolean isExcludeSource() {
-        return excludeSource;
-    }
-
-    public void setExcludeSource(boolean excludeSource) {
-        this.excludeSource = excludeSource;
-    }
+    private final boolean excludeSource = true;
 
     public final static String SEPARATOR_LINE = EmblTag.XX_TAG  + "\n";
     public final static String TERMINATOR_LINE = EmblTag.TERMINATOR_TAG  + "\n";
@@ -39,50 +36,47 @@ public class EmblReducedFlatFileWriter extends EntryWriter {
         if (entry == null) {
             return false;
         }
+
+        if (entry.getSequence() == null || entry.getSequence().getSequenceByte() == null) {
+            throw new IOException(REDUCED_FF_WRITE_FAILED_MISSING_SEQUENCE + ": " + entry.getPrimaryAccession());
+        }
+
+        if (entry.getDataClass() == Entry.CON_DATACLASS && !entry.isContigs()) {
+            throw new IOException(REDUCED_FF_WRITE_FAILED_MISSING_CONTIGS + ": " + entry.getPrimaryAccession());
+        }
+
+        if (entry.getDataClass() != Entry.CON_DATACLASS && entry.isContigs()) {
+            throw new IOException(REDUCED_FF_WRITE_FAILED_UNEXPECTED_CONTIGS + ": " + entry.getPrimaryAccession());
+        }
+
         if(new IDWriter(entry).write(writer)) {
             writer.write(SEPARATOR_LINE);
         }
 
-        //Not expanded entry
-        if( null == entry.getSequence()
-                || null == entry.getSequence().getSequenceByte()||entry.isNonExpandedCON() )
-            writeFeatures( writer );
+        if (entry.getDataClass() == Entry.CON_DATACLASS) {
 
-        if(new COWriter(entry, wrapType).write(writer)) {
-            if (entry.getSequence() != null &&
-                    entry.getSequence().getSequenceByte() != null&&!entry.isNonExpandedCON()) {
+            boolean expandedCON = isExpandedCON();
+            boolean notExpandedCON = isNotExpandedCON(); // in theory should be equal to !expandedCON
+
+            //Expanded entry: CO line, Feature, SQ line
+            if (expandedCON) {
+                new COWriter(entry, wrapType).write(writer);
                 writer.write(SEPARATOR_LINE);
+                writeFeatures(writer);
+                new EmblSequenceWriter(entry, entry.getSequence()).write(writer);
+            }
+
+            //Not expanded entry: Feature, CO line
+            if (notExpandedCON) {
+                writeFeatures(writer);
+                new COWriter(entry, wrapType).write(writer);
             }
         }
-
-        //Expanded entry
-        if( null != entry.getSequence()
-                && null != entry.getSequence().getSequenceByte()&&!entry.isNonExpandedCON() )
-            writeFeatures( writer );
-
-        // TODO; check -- these master writers are probably not needed
-        if(new MasterWGSWriter(entry, wrapType).write(writer)) {
-            writer.write(SEPARATOR_LINE);
-        }
-        if(new MasterCONWriter(entry, wrapType).write(writer)) {
-            writer.write(SEPARATOR_LINE);
-        }
-
-        if((new MasterTPAWriter(entry, wrapType)).write(writer)) {
-            writer.write(SEPARATOR_LINE);
-        }
-        if((new MasterTLSWriter(entry, wrapType)).write(writer)) {
-            writer.write(SEPARATOR_LINE);
-        }
-
-        (new MasterTSAWriter(entry, wrapType)).write(writer);
-
-        if(entry.getMasterScaffoldAccessions() != null && !entry.getMasterScaffoldAccessions().isEmpty()) {
-            new MasterScaffoldWriter(entry, wrapType).write(writer);
-        }
-
-        if(!entry.isNonExpandedCON())
+        else {
+            writeFeatures(writer);
             new EmblSequenceWriter(entry, entry.getSequence()).write(writer);
+        }
+
         writer.write(TERMINATOR_LINE);
 
         writer.flush();
@@ -91,7 +85,7 @@ public class EmblReducedFlatFileWriter extends EntryWriter {
 
     @Override
     public void writeFeatures(Writer writer) throws IOException {
-        if (new FTWriter(entry, isSortFeatures(), isSortQualifiers(), isExcludeSource(), wrapType).write(writer)) {
+        if (new FTWriter(entry, isSortFeatures(), isSortQualifiers(), excludeSource, wrapType).write(writer)) {
             writer.write(SEPARATOR_LINE);
         }
     }
@@ -99,6 +93,14 @@ public class EmblReducedFlatFileWriter extends EntryWriter {
     @Override
     protected void writeReferences(Writer writer) throws IOException {
         // do nothing
+    }
+
+    private boolean isExpandedCON() {
+        return entry.getSequence() != null && entry.getSequence().getSequenceByte() != null && !entry.isNonExpandedCON();
+    }
+
+    private boolean isNotExpandedCON() {
+        return entry.getSequence() == null || entry.getSequence().getSequenceByte() == null || entry.isNonExpandedCON();
     }
 
 }
