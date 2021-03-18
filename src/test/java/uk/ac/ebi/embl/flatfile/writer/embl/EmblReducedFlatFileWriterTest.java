2a65bf4dcf58a0e6e1a3340ad3193b4773004d8e
prabh-t <prabhat@ebi.ac.uk>
2021-03-18 17:03:59 +0000

    added more test cases for reduced ff writer

--- src/test/java/uk/ac/ebi/embl/flatfile/writer/embl/EmblReducedFlatFileWriterTest.java
+++ src/test/java/uk/ac/ebi/embl/flatfile/writer/embl/EmblReducedFlatFileWriterTest.java
@@ -15,10 +15,10 @@
  ******************************************************************************/
 package uk.ac.ebi.embl.flatfile.writer.embl;
 
-import uk.ac.ebi.embl.api.entry.EntryFactory;
 import uk.ac.ebi.embl.api.entry.Text;
 import uk.ac.ebi.embl.api.entry.feature.Feature;
 import uk.ac.ebi.embl.api.entry.feature.FeatureFactory;
+import uk.ac.ebi.embl.api.entry.location.LocalRange;
 import uk.ac.ebi.embl.api.entry.location.LocationFactory;
 import uk.ac.ebi.embl.api.entry.qualifier.QualifierFactory;
 import uk.ac.ebi.embl.api.entry.reference.Article;
@@ -26,11 +26,8 @@ import uk.ac.ebi.embl.api.entry.reference.Reference;
 import uk.ac.ebi.embl.api.entry.reference.ReferenceFactory;
 import uk.ac.ebi.embl.api.entry.sequence.Sequence.Topology;
 import uk.ac.ebi.embl.api.entry.sequence.SequenceFactory;
-import uk.ac.ebi.embl.api.validation.Severity;
-import uk.ac.ebi.embl.api.validation.ValidationResult;
 import uk.ac.ebi.embl.flatfile.FlatFileUtils;
 import uk.ac.ebi.embl.flatfile.reader.embl.EmblLineReader;
-import uk.ac.ebi.embl.flatfile.reader.embl.RAReader;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -39,74 +36,43 @@ import java.io.StringWriter;
 
 public class EmblReducedFlatFileWriterTest extends EmblWriterTest {
 
-	public void testWrite_Entry() throws IOException {
+	protected void setUp() throws Exception {
+		super.setUp();
+		initBaseEntry();
+	}
+
+	private void initBaseEntry() throws IOException {
+		entry.setPrimaryAccession("DP000153");
+		entry.setDataClass("CON");
+		entry.setDivision("MAM");
+		entry.setDescription(new Text("Cloning and characterization of a cDNA encoding a novel subtype of rat thyrotropin-releasing hormone receptor."));
+		entry.setComment(new Text("Cloning\n and characterization\n of a cDNA encoding a novel subtype of rat\n thyrotropin-releasing hormone receptor"));
+		addReference();
+	}
+
+	/**
+	 * test for CON with contigs + features + sequence
+	 * @throws IOException
+	 */
+	public void testCONContigsFeaturesSequence() throws IOException {
+		addFeature();
 		SequenceFactory sequenceFactory = new SequenceFactory();
 		entry.setSequence(sequenceFactory.createSequenceByte("aa".getBytes()));
-		entry.setPrimaryAccession("DP000153");
 		entry.getSequence().setVersion(1);
 		entry.getSequence().setTopology(Topology.LINEAR);
 		entry.getSequence().setMoleculeType("genomic RNA");
-		entry.setDataClass("CON");
-		entry.setDivision("MAM");
 		entry.setIdLineSequenceLength(2);
-		entry.setDescription(new Text("Cloning and characterization of a cDNA encoding a novel subtype of rat thyrotropin-releasing hormone receptor."));
-		entry.setComment(new Text("Cloning\n and characterization\n of a cDNA encoding a novel subtype of rat\n thyrotropin-releasing hormone receptor"));
 
-		ReferenceFactory referenceFactory = new ReferenceFactory();
-		Article article = referenceFactory.createArticle(
-				"Cloning and characterization of a cDNA encoding a novel subtype of rat thyrotropin-releasing hormone receptor",
-				"J. Biol. Chem.");
-		article.setVolume("273");
-		article.setIssue("48");
-		article.setFirstPage("32281");
-		article.setLastPage("32287");
-		article.setYear(FlatFileUtils.getDay("10-SEP-1998"));
-		EmblLineReader lineReader = new EmblLineReader();
-		lineReader.getCache().setPublication(article);
-		lineReader.setReader(new BufferedReader(new StringReader(
-				"RA   Antonellis A., Ayele K., Benjamin B., Blakesley R.W., Boakye A., Bouffard G.G., Brinkley C., Brooks S., Chu G., Coleman H., Engle J., Gestole M., Greene A., Guan X., Gupta J., Haghighi P., Han J., Hansen N., Ho S.-L., Hu P., Hunter G., Hurle B., Idol J.R., Kwong P., Laric P., Larson S., Lee-Lin S.-Q., Legaspi R., Madden M., Maduro Q.L., Maduro V.B., Margulies E.H., Masiello C., Maskeri B., McDowell J., Mojidi H.A., Mullikin J.C., Oestreicher J.S., Park M., Portnoy M.E., Prasad A., Puri O., Reddix-Dugue N., Schandler K., Schueler M.G., Sison C., Stantripop S., Stephen E., Taye A., Thomas J.W., Thomas P.J., Tsipouri V., Ung L., Vogt J.L., Wetherby K.D., Young A., Green E.D.")));
-		lineReader.readLine();
-		ValidationResult result = (new RAReader(lineReader)).read(entry);
-		assertEquals(0, result.count(Severity.ERROR));
-		article.setConsortium("Google consortium");
-		Reference reference = referenceFactory.createReference(article, 1);
-		reference.setComment("reference comment");
-		entry.addReference(reference);
-
-		EntryFactory entryFactory = new EntryFactory();
-		entry.addAssembly(entryFactory.createAssembly("AC004528", 1, 18665l,
-				19090l, true, 1l, 426l));
-		entry.addAssembly(entryFactory.createAssembly("AC004529", 6, 45665l,
-				98790l, true, 6l, 546l));
-		entry.addAssembly(entryFactory.createAssembly("AC004528", 1, 18665l,
-				19090l, true, 1l, 426l));
-		entry.addAssembly(entryFactory.createAssembly("AC004529", 6, 45665l,
-				98790l, true, 6l, 546l));
-
-		FeatureFactory featureFactory = new FeatureFactory();
-		Feature feature = featureFactory.createCdsFeature();
 		LocationFactory locationFactory = new LocationFactory();
-		feature.getLocations().addLocation(
-				locationFactory.createLocalRange(3514l, 4041l, false));
-		QualifierFactory qualifierFactory = new QualifierFactory();
-		feature.addQualifier(qualifierFactory.createQualifier("product",
-				"hypothetical protein"));
-		feature.addQualifier(qualifierFactory.createQualifier("note", "ORF 5"));
-		feature.addQualifier(qualifierFactory.createQualifier("db_xref",
-				"InterPro:IPR001964"));
-		feature.addQualifier(qualifierFactory.createQualifier("db_xref",
-				"UniProtKB/Swiss-Prot:P09511"));
-		feature.addQualifier(qualifierFactory.createQualifier("protein_id",
-				"CAA31466.1"));
-		feature.addQualifier(qualifierFactory.createQualifier("translation",
-				"MEEDDHAGKHDALSALSQWLWSKPLGQHNADLDDDEEVTTGQEELFLPEEQVRARHLFSQKTISREVPAEQSRSGRVYQTARHSLMECSRPTMSIKSQWSFWSSSPKPLPKIPVPSLTSWTHTVNSTPFPQLSTSSGSQSPGKGRLQRLTSTERNGTTLPRTNSGSSTKAMVLHR"));
-		entry.addFeature(feature);
+		LocalRange localRange = locationFactory.createLocalRange(1L, 2L);
+		entry.getSequence().addContig(localRange);
 
 		StringWriter writer = new StringWriter();
 		assertTrue(new EmblReducedFlatFileWriter(entry).write(writer));
-		//System.out.print(writer.toString());
 		assertEquals(
 				"ID   DP000153; SV 1; linear; genomic RNA; CON; MAM; 2 BP.\n" +
+						"XX\n" +
+						"CO   join(1..2)\n" +
 						"XX\n" +
 						"FH   Key             Location/Qualifiers\n" +
 						"FH\n" +
@@ -126,4 +92,173 @@ public class EmblReducedFlatFileWriterTest extends EmblWriterTest {
 						"//\n",
 				writer.toString());
 	}
+
+	/**
+	 * test for non-CON with features + sequence -> check flat file
+	 * @throws IOException
+	 */
+	public void testNonCONFeaturesSequence() throws IOException {
+		entry.setDataClass("SET");
+		addFeature();
+		SequenceFactory sequenceFactory = new SequenceFactory();
+		entry.setSequence(sequenceFactory.createSequenceByte("aa".getBytes()));
+		entry.getSequence().setVersion(1);
+		entry.getSequence().setTopology(Topology.LINEAR);
+		entry.getSequence().setMoleculeType("genomic RNA");
+		entry.setIdLineSequenceLength(2);
+
+		StringWriter writer = new StringWriter();
+		assertTrue(new EmblReducedFlatFileWriter(entry).write(writer));
+		assertEquals(
+				"ID   DP000153; SV 1; linear; genomic RNA; SET; MAM; 2 BP.\n" +
+						"XX\n" +
+						"FH   Key             Location/Qualifiers\n" +
+						"FH\n" +
+						"FT   CDS             3514..4041\n" +
+						"FT                   /product=\"hypothetical protein\"\n" +
+						"FT                   /note=\"ORF 5\"\n" +
+						"FT                   /db_xref=\"InterPro:IPR001964\"\n" +
+						"FT                   /db_xref=\"UniProtKB/Swiss-Prot:P09511\"\n" +
+						"FT                   /protein_id=\"CAA31466.1\"\n" +
+						"FT                   /translation=\"MEEDDHAGKHDALSALSQWLWSKPLGQHNADLDDDEEVTTGQEEL\n" +
+						"FT                   FLPEEQVRARHLFSQKTISREVPAEQSRSGRVYQTARHSLMECSRPTMSIKSQWSFWSS\n" +
+						"FT                   SPKPLPKIPVPSLTSWTHTVNSTPFPQLSTSSGSQSPGKGRLQRLTSTERNGTTLPRTN\n" +
+						"FT                   SGSSTKAMVLHR\"\n" +
+						"XX\n" +
+						"SQ   Sequence 2 BP; 2 A; 0 C; 0 G; 0 T; 0 other;\n" +
+						"     aa                                                                        2\n" +
+						"//\n",
+				writer.toString());
+	}
+
+	/**
+	 * test for CON with NO contigs + features + sequence
+	 * @throws IOException
+	 */
+	public void testCONNoContigsFeaturesSequence()  {
+		addFeature();
+		SequenceFactory sequenceFactory = new SequenceFactory();
+		entry.setSequence(sequenceFactory.createSequenceByte("aa".getBytes()));
+		entry.getSequence().setVersion(1);
+		entry.getSequence().setTopology(Topology.LINEAR);
+		entry.getSequence().setMoleculeType("genomic RNA");
+		entry.setIdLineSequenceLength(2);
+
+		try {
+			StringWriter writer = new StringWriter();
+			new EmblReducedFlatFileWriter(entry).write(writer);
+			fail( "Expected IOException: " + EmblReducedFlatFileWriter.REDUCED_FF_WRITE_FAILED_MISSING_CONTIGS);
+		} catch (IOException exception) {
+			assertTrue(exception.getMessage().startsWith(EmblReducedFlatFileWriter.REDUCED_FF_WRITE_FAILED_MISSING_CONTIGS));
+		}
+	}
+
+	/**
+	 * test for non-CON with contigs + features + sequence
+	 * @throws IOException
+	 */
+	public void testNonCONContigsFeaturesSequence() throws IOException {
+		entry.setDataClass("SET");
+		addFeature();
+		SequenceFactory sequenceFactory = new SequenceFactory();
+		entry.setSequence(sequenceFactory.createSequenceByte("aa".getBytes()));
+		entry.getSequence().setVersion(1);
+		entry.getSequence().setTopology(Topology.LINEAR);
+		entry.getSequence().setMoleculeType("genomic RNA");
+		entry.setIdLineSequenceLength(2);
+
+		LocationFactory locationFactory = new LocationFactory();
+		LocalRange localRange = locationFactory.createLocalRange(1L, 2L);
+		entry.getSequence().addContig(localRange);
+
+		try {
+			StringWriter writer = new StringWriter();
+			new EmblReducedFlatFileWriter(entry).write(writer);
+			fail( "Expected IOException: " + EmblReducedFlatFileWriter.REDUCED_FF_WRITE_FAILED_UNEXPECTED_CONTIGS);
+		} catch (IOException exception) {
+			assertTrue(exception.getMessage().startsWith(EmblReducedFlatFileWriter.REDUCED_FF_WRITE_FAILED_UNEXPECTED_CONTIGS));
+		}
+	}
+
+	/**
+	 * test without sequence
+	 */
+	public void testNoSequence() {
+		addFeature();
+		entry.setSequence(null);
+		try {
+			StringWriter writer = new StringWriter();
+			new EmblReducedFlatFileWriter(entry).write(writer);
+			fail( "Expected IOException: " + EmblReducedFlatFileWriter.REDUCED_FF_WRITE_FAILED_MISSING_SEQUENCE);
+		} catch (IOException exception) {
+			assertTrue(exception.getMessage().startsWith(EmblReducedFlatFileWriter.REDUCED_FF_WRITE_FAILED_MISSING_SEQUENCE));
+		}
+	}
+
+	/**
+	 * test without features
+	 */
+	public void testNoFeatures() throws IOException {
+		entry.setDataClass("SET");
+		SequenceFactory sequenceFactory = new SequenceFactory();
+		entry.setSequence(sequenceFactory.createSequenceByte("aa".getBytes()));
+		entry.getSequence().setVersion(1);
+		entry.getSequence().setTopology(Topology.LINEAR);
+		entry.getSequence().setMoleculeType("genomic RNA");
+		entry.setIdLineSequenceLength(2);
+
+		StringWriter writer = new StringWriter();
+		assertTrue(new EmblReducedFlatFileWriter(entry).write(writer));
+		assertEquals(
+				"ID   DP000153; SV 1; linear; genomic RNA; SET; MAM; 2 BP.\n" +
+						"XX\n" +
+						"SQ   Sequence 2 BP; 2 A; 0 C; 0 G; 0 T; 0 other;\n" +
+						"     aa                                                                        2\n" +
+						"//\n",
+				writer.toString());
+	}
+
+	private void addReference() throws IOException {
+		ReferenceFactory referenceFactory = new ReferenceFactory();
+		Article article = referenceFactory.createArticle(
+				"Cloning and characterization of a cDNA encoding a novel subtype of rat thyrotropin-releasing hormone receptor",
+				"J. Biol. Chem.");
+		article.setVolume("273");
+		article.setIssue("48");
+		article.setFirstPage("32281");
+		article.setLastPage("32287");
+		article.setYear(FlatFileUtils.getDay("10-SEP-1998"));
+
+		EmblLineReader lineReader = new EmblLineReader();
+		lineReader.getCache().setPublication(article);
+		lineReader.setReader(new BufferedReader(new StringReader(
+				"RA   Antonellis A., Ayele K., Benjamin B., Blakesley R.W., Boakye A., Bouffard G.G., Brinkley C., Brooks S., Chu G., Coleman H., Engle J., Gestole M., Greene A., Guan X., Gupta J., Haghighi P., Han J., Hansen N., Ho S.-L., Hu P., Hunter G., Hurle B., Idol J.R., Kwong P., Laric P., Larson S., Lee-Lin S.-Q., Legaspi R., Madden M., Maduro Q.L., Maduro V.B., Margulies E.H., Masiello C., Maskeri B., McDowell J., Mojidi H.A., Mullikin J.C., Oestreicher J.S., Park M., Portnoy M.E., Prasad A., Puri O., Reddix-Dugue N., Schandler K., Schueler M.G., Sison C., Stantripop S., Stephen E., Taye A., Thomas J.W., Thomas P.J., Tsipouri V., Ung L., Vogt J.L., Wetherby K.D., Young A., Green E.D.")));
+		lineReader.readLine();
+		article.setConsortium("Google consortium");
+		Reference reference = referenceFactory.createReference(article, 1);
+		reference.setComment("reference comment");
+		entry.addReference(reference);
+	}
+
+	public void addFeature() {
+		FeatureFactory featureFactory = new FeatureFactory();
+		Feature feature = featureFactory.createCdsFeature();
+		LocationFactory locationFactory = new LocationFactory();
+		feature.getLocations().addLocation(
+				locationFactory.createLocalRange(3514l, 4041l, false));
+		QualifierFactory qualifierFactory = new QualifierFactory();
+		feature.addQualifier(qualifierFactory.createQualifier("product",
+				"hypothetical protein"));
+		feature.addQualifier(qualifierFactory.createQualifier("note", "ORF 5"));
+		feature.addQualifier(qualifierFactory.createQualifier("db_xref",
+				"InterPro:IPR001964"));
+		feature.addQualifier(qualifierFactory.createQualifier("db_xref",
+				"UniProtKB/Swiss-Prot:P09511"));
+		feature.addQualifier(qualifierFactory.createQualifier("protein_id",
+				"CAA31466.1"));
+		feature.addQualifier(qualifierFactory.createQualifier("translation",
+				"MEEDDHAGKHDALSALSQWLWSKPLGQHNADLDDDEEVTTGQEELFLPEEQVRARHLFSQKTISREVPAEQSRSGRVYQTARHSLMECSRPTMSIKSQWSFWSSSPKPLPKIPVPSLTSWTHTVNSTPFPQLSTSSGSQSPGKGRLQRLTSTERNGTTLPRTNSGSSTKAMVLHR"));
+		entry.addFeature(feature);
+	}
+
 }
