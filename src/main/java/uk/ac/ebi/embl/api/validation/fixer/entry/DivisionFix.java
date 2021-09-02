5678bf9abab501d18d02420599dc4e9a899b3ca1
Rajkumar-D <rajkumar.devraj@gmail.com>
2021-09-02 11:48:54 +0530

    ENA-4500: Update for non ncbi division

--- src/main/java/uk/ac/ebi/embl/api/validation/fixer/entry/DivisionFix.java
+++ src/main/java/uk/ac/ebi/embl/api/validation/fixer/entry/DivisionFix.java
@@ -92,7 +92,6 @@ public class DivisionFix extends EntryValidationCheck {
         if(empty(entry.getDivision())) {
             return true;
         }else {
-            
             // If division is NOT empty
             if (getValidationScope().equals(ValidationScope.NCBI) || getValidationScope().equals(ValidationScope.NCBI_MASTER)){
                 // Do NOT set division for NCBI or NCBI_MASTER ValidationScope.
