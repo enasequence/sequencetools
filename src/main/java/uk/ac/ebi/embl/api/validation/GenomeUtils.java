3ffa6f9079fbf109cc3219f9fe4c57b58f9a66cc
prabh-t <prabhat@ebi.ac.uk>
2021-09-09 16:43:50 +0100

    made class final with private constructor

--- src/main/java/uk/ac/ebi/embl/api/validation/GenomeUtils.java
+++ src/main/java/uk/ac/ebi/embl/api/validation/GenomeUtils.java
@@ -4,9 +4,12 @@ import uk.ac.ebi.embl.api.entry.AssemblySequenceInfo;
 
 import java.util.*;
 
-public class GenomeUtils {
+public final class GenomeUtils {
 
     public final static Long COVID_19_OUTBREAK_GENOME_MAX_SIZE = 31000L; //bp
+
+    private GenomeUtils() {
+    }
 
     public static Long calculateGenomeSize(Map<String, AssemblySequenceInfo> sequenceInfo, Set<String> agpPlacedComponents) throws ValidationEngineException {
         long genomeSize = 0;
@@ -14,14 +17,15 @@ public class GenomeUtils {
             AssemblySequenceInfo info = entry.getValue();
 
             if (info.getAssemblyLevel() == 0 || info.getAssemblyLevel() == 1) {
-                if (!agpPlacedComponents.contains(entry.getKey())) {
+                if (!agpPlacedComponents.contains(entry.getKey().toUpperCase())) {
                     genomeSize += info.getSequenceLength();
                 }
             } else if (info.getAssemblyLevel() == 2) {
                 genomeSize += info.getSequenceLength();
-            } else throw new ValidationEngineException("Unexpected assembly level", ValidationEngineException.ReportErrorType.VALIDATION_ERROR);
+            } else
+                throw new ValidationEngineException("Unexpected assembly level", ValidationEngineException.ReportErrorType.VALIDATION_ERROR);
         }
-        return(genomeSize);
+        return (genomeSize);
     }
 
 }
