31d19b5224ba544d77e680ec56e1366900bbbed2
Rajkumar-D <rajkumar.devraj@gmail.com>
2023-01-11 15:01:46 +0000

    ENA-5366: Updating the init of WebinSampleRetrievalService to validate(). from templateProcess this method is called directly so init must happen here

--- src/main/java/uk/ac/ebi/embl/api/validation/submission/SubmissionValidator.java
+++ src/main/java/uk/ac/ebi/embl/api/validation/submission/SubmissionValidator.java
@@ -41,6 +41,9 @@ public class SubmissionValidator implements Validator<Manifest,ValidationRespons
     }
 
     public void validate() throws ValidationEngineException {
+
+        // Initialise SampleRetrievalService.
+        SequenceToolsServices.init(new WebinSampleRetrievalService(options.webinAuthToken.get(),options.webinCliTestMode));
         
         ValidationResult validationResult = new SubmissionValidationPlan(options).execute();
         if (!options.isWebinCLI && !validationResult.isValid()) {
@@ -67,10 +70,6 @@ public class SubmissionValidator implements Validator<Manifest,ValidationRespons
         try {
             options = mapManifestToSubmissionOptions(manifest);
             FeatureReader.isWebinCli = true;
-
-            // Initialise SampleRetrievalService.
-            SequenceToolsServices.init(new WebinSampleRetrievalService(options.webinAuthToken.get(),options.webinCliTestMode));
-
             validate();
         } catch (ValidationEngineException vee) {
             switch (vee.getErrorType()) {
