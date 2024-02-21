b60752b9cd3fa4a12a881ae9d763d348d1ecb7a2
rasko-leinonen-veil-ai <156655213+rasko-leinonen-veil-ai@users.noreply.github.com>
2024-02-21 10:47:17 +0000

    Fix to source location transgenic and overlap check.

--- src/main/java/uk/ac/ebi/embl/api/validation/check/sourcefeature/SequenceCoverageCheck.java
+++ src/main/java/uk/ac/ebi/embl/api/validation/check/sourcefeature/SequenceCoverageCheck.java
@@ -11,12 +11,12 @@
 package uk.ac.ebi.embl.api.validation.check.sourcefeature;
 
 import java.util.*;
+import java.util.function.Predicate;
 
 import uk.ac.ebi.embl.api.entry.Entry;
 import uk.ac.ebi.embl.api.entry.feature.Feature;
 import uk.ac.ebi.embl.api.entry.feature.SourceFeature;
 import uk.ac.ebi.embl.api.entry.location.Location;
-import uk.ac.ebi.embl.api.entry.sequence.Sequence;
 import uk.ac.ebi.embl.api.validation.*;
 import uk.ac.ebi.embl.api.validation.annotation.Description;
 import uk.ac.ebi.embl.api.validation.annotation.ExcludeScope;
@@ -32,18 +32,16 @@ import uk.ac.ebi.embl.api.validation.check.entry.EntryValidationCheck;
 @ExcludeScope(validationScope = {ValidationScope.ASSEMBLY_MASTER, ValidationScope.NCBI_MASTER})
 public class SequenceCoverageCheck extends EntryValidationCheck {
 
-    private static final String MESSAGE_ID_SHORT = "SequenceCoverageCheck-1";
-    private static final String MESSAGE_ID_LONG = "SequenceCoverageCheck-2";
-    private static final String MESSAGE_ID_FIRST_BASE_ERROR = "SequenceCoverageCheck-3";
+    private static final String MESSAGE_ID_INVALID_START_POSITION = "SequenceCoverageCheck-2";
+    private static final String MESSAGE_ID_INVALID_END_POSITION = "SequenceCoverageCheck-1";
     private static final String MESSAGE_ID_TRANSGENIC_SINGLE_SOURCE = "SequenceCoverageCheck-4";
-    private static final String MESSAGE_ID_GAPS_IN_LOCATIONS = "SequenceCoverageCheck-5";
+    private static final String MESSAGE_ID_GAP_BETWEEN_LOCATIONS = "SequenceCoverageCheck-5";
     private static final String MESSAGE_ID_INVALID_CONTIG_LOCATIONS = "SequenceCoverageCheck-6";
     private static final String MESSAGE_ID_TRANSGENIC_SEQUENCE_COVERAGE = "SequenceCoverageCheck-7";
-    private static final String MESSAGE_ID_TRANSGENIC_FOCUS_OCCURRENCE = "SequenceCoverageCheck-8";
-    private static final String MESSAGE_ID_LOCATIONS_OVERLAP = "SequenceCoverageCheck-9";
 
-    private static final int BEGIN_POS = 0;
-    private static final int END_POS = 1;
+    private static final String MESSAGE_ID_SEQUENCE_COVERAGE = "SequenceCoverageCheck-3";
+    private static final String MESSAGE_ID_TRANSGENIC_FOCUS_OCCURRENCE = "SequenceCoverageCheck-8";
+    private static final String MESSAGE_ID_LOCATION_OVERLAP = "SequenceCoverageCheck-9";
 
     /**
      * Checks the coverage of sequence by source features' locations.
@@ -57,161 +55,191 @@ public class SequenceCoverageCheck extends EntryValidationCheck {
         if (entry == null || entry.getSequence() == null) {
             return result;
         }
-        // checks the CONTIG/CO line locations cover the sequence length
-        if (entry.getSequence().getContigs() != null && entry.getSequence().getContigs().size() != 0) {
-            result = checkContigLocation(entry);
-        }
 
-        // collect all sources
+        validateContigsFullCoverage(entry);
+
         List<SourceFeature> sources = new ArrayList<>();
         for (Feature feature : entry.getFeatures()) {
             if (feature instanceof SourceFeature) {
-                sources.add((SourceFeature) feature);
+                SourceFeature source = (SourceFeature) feature;
+                sources.add(source);
             }
         }
-
         if (sources.isEmpty()) {
             return result;
         }
 
-        // checks sequence
-        Sequence sequence = entry.getSequence();
-        if (sequence == null) {
+        Origin firstSourceOrigin = sources.get(0).getOrigin();
+
+        if (sources.stream().filter(source -> source.isTransgenic() || source.isFocus()).count() > 1) {
+            // Only one /focus or /transgenic qualifier is allowed.
+            reportError(firstSourceOrigin, MESSAGE_ID_TRANSGENIC_FOCUS_OCCURRENCE);
             return result;
         }
 
-        long sequenceLength = sequence.getLength();
+        long sequenceLength = entry.getSequence().getLength();
 
+        if (sources.stream().anyMatch(SourceFeature::isTransgenic)) {
+            // Validate sources when transgenic qualifier is present.
+            //
 
-    /* Transgenic sequences are validated differently. They have at least two source feature keys and
-       the source feature key having the /transgenic qualifier must span the whole sequence. The /focus and
-       /transgenic qualifiers are mutually exclusive.
-     */
-
-        boolean hasTransgenic = false;
-        boolean hasFocus = false;
-        Origin firstSourceOrigin = sources.get(0).getOrigin();
-
-        for (SourceFeature source : sources) {
-            if (source.isTransgenic() || source.isFocus()) {
-                if (hasTransgenic || hasFocus) {
-                    reportError(source.getOrigin(), MESSAGE_ID_TRANSGENIC_FOCUS_OCCURRENCE);
-                }
-
-                if (source.isTransgenic()) {
-                    hasTransgenic = true;
-                    if (sequenceLength > source.getLength())
-                        reportError(source.getOrigin(), MESSAGE_ID_TRANSGENIC_SEQUENCE_COVERAGE);
-                }
-
-                if (source.isFocus()) {
-                    hasFocus = true;
-                }
-            }
-        }
-
-        if (hasTransgenic && sources.size() == 1) {
-            reportError(firstSourceOrigin, MESSAGE_ID_TRANSGENIC_SINGLE_SOURCE);
-        }
-
-        if (!hasTransgenic) {
-            // Sort locations by begin position.
-            List<Location> locations = new ArrayList<>();
-            for (SourceFeature source : sources) {
-                locations.addAll(source.getLocations().getLocations());
-            }
-            Collections.sort(locations, Comparator.comparing(Location::getBeginPosition));
-
-            Iterator<Location> locationIter = locations.iterator();
-            Long[] position = getPosition(locationIter.next());
-
-            // checks first location
-            if (position[BEGIN_POS] != 1) {
-                result.append(
-                        EntryValidations.createMessage(
-                                firstSourceOrigin, Severity.ERROR, MESSAGE_ID_FIRST_BASE_ERROR, position[BEGIN_POS]));
+            // At least one other source is required with /transgenic.
+            if (sources.size() == 1) {
+                reportError(firstSourceOrigin, MESSAGE_ID_TRANSGENIC_SINGLE_SOURCE);
                 return result;
             }
 
-            while (locationIter.hasNext()) {
-                Long[] prevPosition = position;
-                position = getPosition(locationIter.next());
-                if ((prevPosition[END_POS] + 1) != position[BEGIN_POS]) { // not contiguous
-                    if ((prevPosition[END_POS] + 1) > position[BEGIN_POS]) {
-                        reportCoverageError(
-                                result, firstSourceOrigin, MESSAGE_ID_LOCATIONS_OVERLAP, 0, sequenceLength);
-                    } else {
-                        reportCoverageError(
-                                result, firstSourceOrigin, MESSAGE_ID_GAPS_IN_LOCATIONS, 0, sequenceLength);
-                    }
-                    return result;
-                }
+            // Transgenic source must cover the whole sequence.
+            List<Location> transgenicLocations = sortedLocations(sources, SourceFeature::isTransgenic);
+            if (!validateFullCoverageNoGapsOrOverlaps(transgenicLocations, sequenceLength, firstSourceOrigin)) {
+                reportError(firstSourceOrigin, MESSAGE_ID_TRANSGENIC_SEQUENCE_COVERAGE);
+                return result;
             }
 
-            if (position[END_POS] > sequenceLength) {
-                // Last location end position is after sequence length.
-                reportCoverageError(
-                        result, firstSourceOrigin, MESSAGE_ID_LONG, position[END_POS], sequenceLength);
-            } else if (position[END_POS] < sequenceLength) {
-                // Last location end position is before sequence length.
-                reportCoverageError(
-                        result, firstSourceOrigin, MESSAGE_ID_SHORT, position[END_POS], sequenceLength);
-            }
-        }
-
-        return result;
-    }
-
-    /**
-     * Returns begin and end position in correct order.
-     */
-    private Long[] getPosition(Location location) {
-        Long[] positions = new Long[2];
-        if (location.isComplement()) {
-            positions[BEGIN_POS] = location.getEndPosition();
-            positions[END_POS] = location.getBeginPosition();
+            // Other sources must not overlap.
+            List<Location> nonTransgenicLocations = sortedLocations(sources, s -> !s.isTransgenic());
+            validateWithinSequenceNoOverlaps(nonTransgenicLocations, sequenceLength, firstSourceOrigin);
         } else {
-            positions[BEGIN_POS] = location.getBeginPosition();
-            positions[END_POS] = location.getEndPosition();
-        }
-        return positions;
-    }
+            // Validate sources when transgenic qualifier is not present.
+            //
 
-    /**
-     * Adds error to the result.
-     *
-     * @param result a reference to validation result
-     * @param origin the origin
-     */
-    private void reportCoverageError(
-            ValidationResult result, Origin origin, String messageId, long coverage, long actualLength) {
-        result.append(
-                EntryValidations.createMessage(origin, Severity.ERROR, messageId, coverage, actualLength));
-    }
-
-    /*
-     * checks the CONTIG/CO line locations cover the sequence length
-     */
-
-    private ValidationResult checkContigLocation(Entry entry) {
-        List<Location> locations = entry.getSequence().getContigs();
-        Long contigSequenceCoverLength = 0L;
-        Long sequenceLength = entry.getSequence().getLength();
-        for (Location location : locations) {
-            contigSequenceCoverLength += location.getLength();
-        }
-
-        if (!sequenceLength.equals(contigSequenceCoverLength)) {
-
-            result.append(
-                    EntryValidations.createMessage(
-                            entry.getSequence().getContigs().get(0).getOrigin(),
-                            Severity.ERROR,
-                            MESSAGE_ID_INVALID_CONTIG_LOCATIONS,
-                            contigSequenceCoverLength,
-                            sequenceLength));
+            // Sources must cover the whole sequence and must not overlap.
+            List<Location> allLocations = sortedLocations(sources);
+            if (!validateFullCoverageNoGapsOrOverlaps(allLocations, sequenceLength, firstSourceOrigin)) {
+                reportError(firstSourceOrigin, MESSAGE_ID_SEQUENCE_COVERAGE);
+            }
         }
 
         return result;
+    }
+
+    /**
+     * Validate that contigs cover the whole sequence.
+     */
+    private void validateContigsFullCoverage(Entry entry) {
+        if (entry.getSequence().getContigs() != null && entry.getSequence().getContigs().size() != 0) {
+            List<Location> locations = entry.getSequence().getContigs();
+            long contigCoverageLength = 0L;
+            for (Location location : locations) {
+                contigCoverageLength += location.getLength();
+            }
+            long sequenceLength = entry.getSequence().getLength();
+            if (sequenceLength != contigCoverageLength) {
+                Origin firstContigOrigin = entry.getSequence().getContigs().get(0).getOrigin();
+                reportError(firstContigOrigin,
+                        MESSAGE_ID_INVALID_CONTIG_LOCATIONS,
+                        contigCoverageLength,
+                        sequenceLength);
+            }
+        }
+    }
+
+    private static List<Location> sortedLocations(List<SourceFeature> sources) {
+        return sortedLocations(sources, e -> true);
+    }
+
+    private static List<Location> sortedLocations(List<SourceFeature> sources, Predicate<SourceFeature> add) {
+        List<Location> locations = new ArrayList<>();
+        for (SourceFeature source : sources) {
+            if (add.test(source)) {
+                locations.addAll(source.getLocations().getLocations());
+            }
+        }
+        locations.sort(Comparator.comparing(Location::getBeginPosition));
+        return locations;
+    }
+
+    private void validateWithinSequenceNoOverlaps(List<Location> locations, long sequenceLength, Origin firstSourceOrigin) {
+        Iterator<Location> locationIter = locations.iterator();
+        Location location = locationIter.next();
+        if (!validateStartPositionWithinSequence(location, sequenceLength, firstSourceOrigin)) {
+            return;
+        }
+        while (locationIter.hasNext()) {
+            Location prevLocation = location;
+            location = locationIter.next();
+            if (!validateNoOverlapBetweenLocations(prevLocation, location, firstSourceOrigin)) {
+                return;
+            }
+        }
+        validateEndPositionWithinSequence(location, sequenceLength, firstSourceOrigin);
+    }
+
+    /** Returns false if the locations do not cover the whole sequence.
+     */
+    private boolean validateFullCoverageNoGapsOrOverlaps(List<Location> locations, long sequenceLength, Origin firstSourceOrigin) {
+        Iterator<Location> itr = locations.iterator();
+        Location location = itr.next();
+        if (!validateStartPositionIsOne(location, firstSourceOrigin)) {
+            return false;
+        }
+        while (itr.hasNext()) {
+            Location prevLocation = location;
+            location = itr.next();
+            if (!validateNoGapBetweenLocations(prevLocation, location, firstSourceOrigin)) {
+                return false;
+            }
+            validateNoOverlapBetweenLocations(prevLocation, location, firstSourceOrigin);
+        }
+        if (!validateEndPositionIsSequenceLength(location, sequenceLength, firstSourceOrigin)) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validateStartPositionIsOne(Location location, Origin firstSourceOrigin) {
+        if (minPosition(location) != 1) {
+            reportError(firstSourceOrigin, MESSAGE_ID_INVALID_START_POSITION, minPosition(location));
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validateStartPositionWithinSequence(Location location, long sequenceLength, Origin firstSourceOrigin) {
+        if (minPosition(location) < 1 || minPosition(location) > sequenceLength) {
+            reportError(firstSourceOrigin, MESSAGE_ID_INVALID_START_POSITION, minPosition(location));
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validateEndPositionIsSequenceLength(Location location, long sequenceLength, Origin firstSourceOrigin) {
+        if (maxPosition(location) != sequenceLength) {
+            reportError(firstSourceOrigin, MESSAGE_ID_INVALID_END_POSITION, maxPosition(location), sequenceLength);
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validateEndPositionWithinSequence(Location location, long sequenceLength, Origin firstSourceOrigin) {
+        if (maxPosition(location) > sequenceLength) {
+            reportError(firstSourceOrigin, MESSAGE_ID_INVALID_END_POSITION, maxPosition(location), sequenceLength);
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validateNoGapBetweenLocations(Location prevLocation, Location location, Origin firstSourceOrigin) {
+        if ((maxPosition(prevLocation) + 1) < minPosition(location)) {
+            reportError(firstSourceOrigin, MESSAGE_ID_GAP_BETWEEN_LOCATIONS);
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validateNoOverlapBetweenLocations(Location prevLocation, Location location, Origin firstSourceOrigin) {
+        if ((maxPosition(prevLocation) + 1) > minPosition(location)) {
+            reportError(firstSourceOrigin, MESSAGE_ID_LOCATION_OVERLAP);
+            return false;
+        }
+        return true;
+    }
+
+    private static long minPosition(Location location) {
+        return location.getBeginPosition() <= location.getEndPosition() ? location.getBeginPosition() : location.getEndPosition();
+    }
+
+    private static long maxPosition(Location location) {
+        return location.getBeginPosition() >= location.getEndPosition() ? location.getBeginPosition() : location.getEndPosition();
     }
 }
